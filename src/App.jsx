64c1d53e47b03bbbb8c593745b185d60e5b6c186
robert tuke <67240736+Robert6353@users.noreply.github.com>
2023-03-24 10:16:10 +0000

    this

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <main>
+      React ⚛️ + Vite ⚡ + Replit 🌀
+      <new>
+        Hello there
+      </new>
+      <h1>
+        How are you?
+      </h1>
+      <div>
+        <header class-name = "app-header">
+          <img src = {logo} className 
+            = "App-Logo" alt = "logo"
+        </header>
+      </div>
+    </main>
+  )
+}
